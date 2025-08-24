ab96d6945b57c1ada2d118a8ab0b4ebb1be620c7
msaroshali <muhammadsaroshali@gmail.com>
2025-08-24 21:14:16 +0200

    controller

---
+++ src/main/java/aboutNew/controller/TweetController.java
@@ -0,0 +1,47 @@
+package aboutNew.controller;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import aboutNew.Obtainer;
+import aboutNew.dao.TweetDAO;
+import aboutNew.model.Tweet;
+import io.javalin.http.Context;
+
+public class TweetController {
+
+    public static void search(Context ctx)
+    {
+        String keyword = ctx.queryParam("keyword");
+        String username = ctx.queryParam("username");
+
+        try {
+
+            List<Map<String, String>> rawTweets = Obtainer.getTweets(keyword, username);
+            // convert to model
+            List<Tweet> fetchedTweets = new ArrayList<>();
+            for(Map<String, String> map : rawTweets)
+            {
+                String date = map.getOrDefault("date","");
+                String content = map.getOrDefault("content", "");
+                Tweet t = new Tweet(username, content, date);
+                fetchedTweets.add(t);   
+            }
+
+            System.out.println("Saving tweets to database...");
+
+            for (Tweet t : fetchedTweets) {
+                TweetDAO.saveTweet(t);
+            }
+            System.out.println("Tweets saved to database successfully.");
+
+            // Return JSON to frontend
+            ctx.json(rawTweets);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            ctx.status(500).json(Map.of("error", "Internal Server Error: " + e.getMessage()));
+        }
+    }
+
+}
