0314c521db894395d69cb1a228736b3ac0454217
msaroshali <muhammadsaroshali@gmail.com>
2026-05-31 18:27:24 +0200

    funny chars in tweets and more news sources

--- src/main/java/aboutNew/Obtainer.java
+++ src/main/java/aboutNew/Obtainer.java
@@ -2,6 +2,7 @@ package aboutNew;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -36,7 +37,7 @@ public class Obtainer {
         // pb.redirectErrorStream(true);
         Process process = pb.start();
     
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
         StringBuilder output = new StringBuilder();
         String line;
         while ((line = reader.readLine()) != null) {
